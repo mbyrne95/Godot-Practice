7811d7a23414303f3f5d6cf3db77c83ba099c72c
Matt Byrne <matthewtbyrne1@gmail.com>
2024-01-26 23:56:06 -0500

    boss behavior refactor
    -refactored the bones of boss behavior, making adding new behaviors/tweaking existing much easier

--- Characters/Enemies/Bosses/eye_boss/boss_practice.gd
+++ Characters/Enemies/Bosses/eye_boss/boss_practice.gd
@@ -11,11 +11,16 @@ var MAX_HEALTH
 
 @onready var aoe_scene = preload("res://Characters/Enemies/Bosses/eye_boss/aoe_practice.tscn")
 
+@onready var random_volley_scene = preload("res://Characters/Enemies/Bosses/eye_boss/random_volley_area.tscn")
+
 @onready var eye_follow = $"Eye Follow"
 var eye_follow_speed = 0.2
 var has_ceased = true
 
-@onready var rotate_shoot_timer = $"rotate shoot clock"
+@onready var ability_timer = $"ability timer"
+var ability_timer_initialized = false
+@onready var attack_timer = $"attack timer"
+var attack_timer_initialized = false
 @onready  var rotator = $"Eye Follow/Rotator2"
 @onready var rotate_speed = 75
 @onready var rotate_spawn_count = 6
@@ -25,10 +30,31 @@ var is_rotate_shooting = false
 var spin = true
 
 var is_attacking
-var attack_list = [
-	0, #corresponds to rotate shoot
-	1, #corresponds to ... 
-]
+var attack_state
+enum attack_list {
+	default,
+	one_shot,
+	stop_player_movement,
+	rotate_shoot,
+	column_attack, ##tentacle attack 
+	volley_attack,	
+}
+
+var hp_ratio
+var hp_state
+enum hp_state_list{
+	high,
+	mid,
+	low
+}
+
+var hits_in_flurry = 2
+var total_flurries = 2
+var time_between_hits = 0.15
+var time_between_flurries = 1.5
+var rotate_shoot_time = 4
+
+var time_between_attacks = 2.0
 
 signal healthChanged(percent_hp)
  
@@ -47,74 +73,82 @@ func _ready():
 	#print(MAX_HEALTH)
 	is_attacking = false
 	player = get_tree().get_first_node_in_group("players")
-	#height = get_tree().get_first_node_in_group("spawn_points").get_node_or_null("boss_height")
 	
 	rotator.rotate_speed = 75
 	rotator.rotate_spawn_count = 6
 	rotator.radius = 5
 	
-	
-#	var step = 2 * PI / rotate_spawn_count
-#	for i in range(rotate_spawn_count):
-#		var spawn_point = Node2D.new()
-#		var pos = Vector2(radius, 0).rotated(step * i)
-#		spawn_point.position = pos
-#		spawn_point.rotation = pos.angle()
-#		rotator.add_child(spawn_point)
+	hp_ratio = HEALTH / MAX_HEALTH
+	hp_state = hp_state_list.high
+	attack_state = attack_list.default
 	
 func _process(_delta):
+	#print(attack_state)
 	rotator.global_position = muzzle.global_position
-	#print(HEALTH)
-#	if spin:
-#		var new_rotation = rotator.rotation_degrees + rotate_speed * delta
-#		rotator.rotation_degrees = fmod(new_rotation, 360)
+
 	if(player == null):
 		return
+
 	muzzle.look_at(player.global_position)
 	
-func _physics_process(_delta):
-	#print(HEALTH / MAX_HEALTH)
-	if allowed_to_move:
-		if(player == null):
-			return
-		else:
-			if !has_ceased:
-				allowed_to_move = false
-				_cease()
-			#top end
-			elif (HEALTH/MAX_HEALTH >= 0.66) && !is_attacking:
-				#print("top")
-				var x = randi_range(0,1)
-				#var x = 0
-				if x == 0:
-					rotate_shoot_init(4,2)
-				else:
-					tentacle_attack(2, 2, 2)
-			elif (HEALTH/MAX_HEALTH < 0.66) && (HEALTH/MAX_HEALTH >= 0.33)  && !is_attacking:
-				#print("mid")
-				projectile_scene = wavey_bullet
-				SHOOT_CD = 0.06
-				rotate_speed = 100
-				rotator.rotate_speed = rotate_speed
-				var x = randi_range(0,1)
-				#var x = 0
-				if x == 0:
-					rotate_shoot_init(5,1)
-				else:
-					tentacle_attack(3, 3, 1)
-			elif (HEALTH/MAX_HEALTH < 0.33)  && !is_attacking:
-				#print("low")
-				SHOOT_CD = 0.04
-				rotator.rotate_speed = rotate_speed
-				var x = randi_range(0,1)
-				#var x = 0
-				if x == 0:
-					rotate_shoot_init(6,0.5)
-				else:
-					tentacle_attack(4,4,0.5)
-		if is_rotate_shooting:
-			rotate_shoot()		
 	
+
+	if allowed_to_move:
+		
+		hp_ratio = HEALTH / MAX_HEALTH
+		if hp_ratio >= 0.66:
+			hp_state = hp_state_list.high
+		elif hp_ratio < 0.66 && hp_ratio >= 0.33:
+			hp_state = hp_state_list.mid
+		elif hp_ratio < 0.33:
+			hp_state = hp_state_list.low
+			
+		match hp_state:
+			hp_state_list.high:	
+				rotator.rotate_speed = 75
+				hits_in_flurry = 2
+				total_flurries = 2
+				time_between_hits = 0.15
+				time_between_flurries = 1.5
+				rotate_shoot_time = 4
+				time_between_attacks = 2.0
+			hp_state_list.mid:
+				SHOOT_CD = 0.06
+				projectile_scene = wavey_bullet
+				rotator.rotate_speed = 110
+				hits_in_flurry = 3
+				total_flurries = 3
+				time_between_hits = 0.15
+				time_between_flurries = 1.2
+				rotate_shoot_time = 4
+				time_between_attacks = 1.5
+			hp_state_list.low:
+				SHOOT_CD = 0.04
+				rotator.rotate_speed = 150
+				hits_in_flurry = 4
+				total_flurries = 4
+				time_between_hits = 0.12
+				time_between_flurries = 0.8
+				rotate_shoot_time = 6
+				time_between_attacks = 0.8
+				
+					
+		match attack_state:
+			attack_list.default:
+				_attack_timer_init(time_between_attacks)
+			attack_list.one_shot:
+				pass
+			attack_list.stop_player_movement:
+				attack_state = attack_list.one_shot
+				_cease()
+			attack_list.rotate_shoot:
+				rotate_shoot()
+				_ability_timer_init(rotate_shoot_time)
+			attack_list.column_attack:
+				attack_state = attack_list.one_shot
+				tentacle_attack(hits_in_flurry,total_flurries,time_between_hits,time_between_flurries)
+				_ability_timer_init((hits_in_flurry * time_between_hits) + (total_flurries * time_between_flurries))
+
 func rotate_shoot():
 	if !shoot_on_cd:
 		shoot_on_cd = true
@@ -126,40 +160,22 @@ func rotate_shoot():
 			projectile_container.add_child(projectile)
 			projectile.position = s.global_position
 			projectile.rotation = s.global_rotation
-			
-			
+
+
 		await get_tree().create_timer(SHOOT_CD).timeout
 		shoot_on_cd = false
 		
-func rotate_shoot_init(time, time_before_next_attack):
-#	var rotator = rotator_scene.instantiate()
-#	rotator.rotate_speed = rotate_speed
-#	rotator.rotate_spawn_count = rotate_spawn_count
-#	rotator.position = muzzle.global_position
-#	add_child(rotator)
-	is_rotate_shooting = true
-	is_attacking = true
-	#time of rotate shoot
-	await get_tree().create_timer(time).timeout
-	is_rotate_shooting = false
-	#rotator.queue_free()
-	
-	#time between being able to attack again
-	await get_tree().create_timer(time_before_next_attack).timeout
-	is_attacking = false
-		
-func tentacle_attack(num_hits_flurry, num_flurry, time_before_next_attack):
-	is_attacking = true
-	var offset_right = 20
-	var offset_left = -20
+func tentacle_attack(num_hits_flurry, num_flurry, time_bw_hits, time_bw_flurries):
+#	is_attacking = true
+	var offset = 30
 	for i in range(num_flurry):
 		var previous_x= -1000
 		for j in range(num_hits_flurry):
 			var t = tentacle_projectile.instantiate()
 			projectile_container.add_child(t)
-			var new_position = randf_range(0,180)
-			while(new_position < previous_x+offset_right && new_position > previous_x+offset_left):
-				new_position = randf_range(0,180)
+			var new_position = randf_range(10,170)
+			while(new_position < previous_x+offset && new_position > previous_x-offset):
+				new_position = randf_range(10,170)
 			previous_x = new_position
 			t.global_position = Vector2(new_position, 320)
 			#print(t.global_position)
@@ -167,13 +183,9 @@ func tentacle_attack(num_hits_flurry, num_flurry, time_before_next_attack):
 			if (new_position > 90):
 				t.scale.x = -1
 			#time between attacks in a flurry
-			await get_tree().create_timer(0.15).timeout
+			await get_tree().create_timer(time_bw_hits).timeout
 		#time between flurries
-		await get_tree().create_timer(1.5).timeout
-	
-	#time between being able to attack again
-	await get_tree().create_timer(time_before_next_attack).timeout
-	is_attacking = false
+		await get_tree().create_timer(time_bw_flurries).timeout
 
 func rotateToTarget(target, delta):
 	var direction = (target.global_position - eye_follow.global_position)
@@ -188,11 +200,11 @@ func take_damage(amount : int):
 		if (HEALTH <= 0):
 			Globs.child_of_wave_died.emit()
 			queue_free()
-			
+
 		enemy_took_damage.emit(amount)
-		
+
 		await get_tree().create_timer(0.1).timeout
-		
+
 		sprite.material.set_shader_parameter("enabled", false)
 
 func _cease():
@@ -233,3 +245,32 @@ func _aoe_blast(time_before_next_attack):
 	await get_tree().create_timer(time_before_next_attack).timeout
 	is_attacking = false
 
+func _random_volley(duration, time_before_next_attack):
+	is_attacking = true
+	var volley_spawn_container = random_volley_scene.instantiate()
+	volley_spawn_container.position = global_position
+	add_child(volley_spawn_container)
+
+func _ability_timer_init(ability_time):
+	if !ability_timer_initialized:
+		ability_timer_initialized = true
+		ability_timer.wait_time = ability_time
+		ability_timer.start()
+		
+func _attack_timer_init(attack_time):
+	if !attack_timer_initialized:
+		print("timer started")
+		attack_timer_initialized = true
+		attack_timer.wait_time = attack_time
+		attack_timer.start()
+
+func _on_ability_timer_timeout():
+	attack_state = attack_list.default
+	ability_timer_initialized = false
+
+func _on_attack_timer_timeout():
+	print("timer stopped")
+	#start at 3, because 0, 1, and 2 are attacks that shouldn't be in rotation
+	#five is in development
+	attack_state = attack_list.values()[randi_range(3,4)]
+	attack_timer_initialized = false
